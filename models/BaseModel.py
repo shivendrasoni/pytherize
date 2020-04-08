d6f78a5b77917a5b27df5d2ce79c0689062e14b8
Shivendra Soni <shivendra.soni@mindtickle.com>
2020-04-09 04:52:50 +0530

    Add base model class

---
+++ models/BaseModel.py
@@ -0,0 +1,12 @@
+import time
+class BaseModel:
+    db = None
+    doc_type = None
+
+    def __init__(self, db=None):
+        if db is None:
+            raise Exception('Database access object : None')
+        self.db = db
+
+    def list_elements(self):
+        self.db.list_elements(self.doc_type)
